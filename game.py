ed6878c56359bae5c909d21726407e7f8a95ce2d
St0neHunter <161620876+St0neHunter@users.noreply.github.com>
2026-02-09 09:37:42 +0100

    changes feat @Klinkuvos (tutorial and point system)

--- game.py
+++ game.py
@@ -1,11 +1,14 @@
 import random
 import time
+
+import pgzero.game
 import pgzrun
 from pgzhelper import *
 import secrets
 import numpy.random as rnd
 
 startMenu = True
+tutorial = False
 win = False
 caught = False
 lastUpdate = float(time.time())
@@ -13,10 +16,20 @@ lastUpdate = float(time.time())
 WIDTH = 600
 HEIGHT = 600
 
-startButton = Actor('gemred')
-startButton.x = WIDTH / 2
-startButton.y = HEIGHT / 2
-startButton.scale = 2
+startButton = Actor('startbutton')
+startButton.x = WIDTH / 2 + 210
+startButton.y = HEIGHT / 2 + 30
+startButton.scale = 5
+
+tutorialButton = Actor('tutorialbutton')
+tutorialButton.x = WIDTH / 2 + 210
+tutorialButton.y = HEIGHT / 2 + 95
+tutorialButton.scale = 2.5
+
+quitButton = Actor('quitbutton')
+quitButton.x = WIDTH / 2 + 210
+quitButton.y = HEIGHT / 2 + 160
+quitButton.scale = 5
 
 background = Actor('background')
 background.x = WIDTH / 2
@@ -27,6 +40,12 @@ background1 = Actor('background')
 background1.x = WIDTH / 2
 background1.y = HEIGHT / 2 - HEIGHT
 background1.scale = 18.75
+
+homescreenbackground = Actor('homescreen0')
+homescreenbackground.x = WIDTH / 2
+homescreenbackground.y = HEIGHT / 2
+homescreenbackground.scale = 12
+homescreenanimation = 0
 
 gerrit = Actor('standing')
 gerrit.x = 400
@@ -50,17 +69,32 @@ props = [dummyprop]
 random.seed(time.time())
 
 def on_mouse_down(pos, button):
-    global startMenu, startButton
+    global startMenu, startButton, tutorialButton, tutorial
     if startButton.collidepoint(pos) and startMenu:
         startMenu = False
+    elif tutorialButton.collidepoint(pos):
+        tutorial = not tutorial
+    elif quitButton.collidepoint(pos):
+        pgzero.game.exit()
 
 holdingLeft = False
 holdingRight = False
 
 def update():
-    global gerrit, walkStage, lastUpdate, score, walkingLane, holdingLeft, holdingRight, hurtTime
+    global gerrit, walkStage, lastUpdate, score, goal, walkingLane, holdingLeft, holdingRight, hurtTime, homescreenanimation, win
 
     if startMenu:
+        if not lastUpdate > float(time.time()) - 0.15:
+            homescreenbackground.image = 'homescreen' + homescreenanimation.__str__()
+            homescreenbackground.scale = 12
+            if homescreenanimation == 19:
+                homescreenanimation = 0
+            homescreenanimation += 1
+            lastUpdate = time.time()
+        return
+
+    if score >= goal:
+        win = True
         return
 
     ### start of keyboard handling ###
@@ -96,7 +130,12 @@ def update():
     for prop in props:
         prop.y += 4
         if prop.colliderect(gerrit):
-            score += 1
+            if prop.image == 'chicken':
+                score += 5
+            elif prop.image == 'porridge':
+                score += 2
+            elif prop.image == 'scheepsbeschuit':
+                score += 1
             props.remove(prop)
         elif prop.y > HEIGHT  + prop.height:
             props.remove(prop)
@@ -107,7 +146,7 @@ def update():
             if prop.colliderect(prop1):
                 props.remove(prop)
             #elif int(prop.y) in range(int(prop1.y) + int(prop1.height), int(prop1.y) - int(prop1.height)):
-             #   props.remove(prop)
+            #    props.remove(prop)
 
     background.y += 4
     background1.y += 4
@@ -116,18 +155,27 @@ def update():
     if background1.y == HEIGHT + HEIGHT / 2:
         background1.y = HEIGHT / 2 - HEIGHT
 
-    randomint = round(rnd.random() * 10)
-    # print(randomint.__str__())
-    if randomint == 10:
-        newprop = Actor("chicken")
-        newprop.y = -100
-        newprop.x = random.choice([walkingLane1pos, walkingLane2pos, walkingLane3pos])
-        newprop.scale = 5
-        props.append(newprop)
 
-
-    if not lastUpdate > float(time.time()) - 0.05: # update gerrit less frequent
-        if hurtTime == 3:
+    if not lastUpdate > float(time.time()) - 0.05: # main game loop runs less frequent
+        if random.randint(0, 200) == 1:
+            newprop = Actor("chicken")
+            newprop.y = -100
+            newprop.x = random.choice([walkingLane1pos, walkingLane2pos, walkingLane3pos])
+            newprop.scale = 5
+            props.append(newprop)
+        elif random.randint(0, 125) == 1:
+            newprop = Actor("porridge")
+            newprop.y = -100
+            newprop.x = random.choice([walkingLane1pos, walkingLane2pos, walkingLane3pos])
+            newprop.scale = 5
+            props.append(newprop)
+        elif random.randint(0, 75) == 1:
+            newprop = Actor("scheepsbeschuit")
+            newprop.y = -100
+            newprop.x = random.choice([walkingLane1pos, walkingLane2pos, walkingLane3pos])
+            newprop.scale = 5
+            props.append(newprop)
+        elif hurtTime == 3:
             hurtTime = 0
         elif hurtTime > 0:
             hurtTime += 1
@@ -159,22 +207,23 @@ def draw():
     for prop in props:
         prop.draw()
 
-    if startMenu:
+    if tutorial:
         screen.fill((103, 190, 217))
         screen.draw.text(
-            'Welcome!',
-            (210, 190),
-            color=(255, 255, 255),
-            fontsize=60
-        )
-        screen.draw.text(
             'In this game you play as Gerrit, a crewmate.\n'
-            'To survive he needs to eat but that\'s not easy',
-            (100, 230),
-            color=(255, 255, 255),
+            'To survive he needs to eat, but that\'s not easy...\n'
+            '\n'
+            'There is three types of food Gerrit can find and eat on the ship\n'
+            'ship\'s biscuit: 1 point\n'
+            'porridge: 2 point\'s\n'
+            'chicken: 5 points BUT this food is not ment for crewmates.\n'
+            'There is a 50% change of Gerrit being caught and losing a warning\n'
+            'and the second time being thrown off the ship.\n',
+            (20, 120),
+            color=(0, 0, 0),
             fontsize=30
         )
-        startButton.draw()
+        tutorialButton.draw()
 
         screen.draw.text(
             'Controls\n'
@@ -182,13 +231,26 @@ def draw():
             ' - Move right   >\n'
             ' - Jump           ^',
             (10, 10),
-            color=(255, 255, 255),
+            color=(0, 0, 0),
             fontsize=30
         )
+        return
+    if startMenu:
+        screen.fill((103, 190, 217))
+        homescreenbackground.draw()
+        screen.draw.text(
+            'Welcome!',
+            (210, 190),
+            color=(0, 0, 0),
+            fontsize=60
+        )
+        startButton.draw()
+        tutorialButton.draw()
+        quitButton.draw()
 
     elif win:
         screen.draw.text(
-            'Gewonnen!',
+            'You won!',
             (450, 200),
             color=(255, 255, 255),
             fontsize=60
